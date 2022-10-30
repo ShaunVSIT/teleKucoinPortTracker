baf96e8d5c39a99ea3df18fd6cd47e74da44a3e6
Shaun Varghese <91268180+ShaunVSIT@users.noreply.github.com>
2022-10-30 16:29:01 +0800

    Create constants.py
    Template for secret variables file

---
+++ constants.py
@@ -0,0 +1,15 @@
+# This file is a template for storing secret variables
+# Replace <<xxx>> with the appropriate secrets
+
+#Tele bot key
+TELE_API_KEY = '<<your tele bot key goes here>>'
+
+#Kucoin keys
+api_key = '<<Kucoin API Key>>'
+api_secret = '<<Kucoin API secret key>>'
+api_passphrase = '<<Kucoin API passphrase>>'
+
+#Tele chat IDs, i'm using these to check if the chat matches those of my tele accounts
+#so i'm the only one who can use the bot
+chatID1 = '<<first acc tele chat ID>>'
+chatID2 = '<<second acc tele chat ID>>'
